54e1ed8abd60f714d399982ed84bbe545cd95227
wariszargardev <wariszargardev@gmail.com>
2022-10-16 11:50:03 +0500

    Add logger to store

--- app/store.js
+++ app/store.js
@@ -1,12 +1,16 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require('../features/cake/cakeSlice')
 const iceCreamReducer = require('../features/icecream/iceCreamSlice')
+const reduxLogger = require('redux-logger')
+
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
     reducer:{
         cake: cakeReducer,
         iceCream: iceCreamReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
